42c38a3d5df6f3166dd14c543f695e37158cdbde
Eng-Rejoice <rejoicemoyo34@gmail.com>
2022-09-13 21:33:48 -0700

    Kommiting

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Print small letter Alpabets
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-int main(void)
+
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+
+	_putchar('\n');
+}
